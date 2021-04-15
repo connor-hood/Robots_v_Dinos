e782c5e3264fa13cf284e21a10bdf9cecc005d51
Connor Hood <connor_hood@yahoo.com>
2021-04-15 15:35:00 -0500

    built 3 dinosaurs

--- Herd.py
+++ Herd.py
@@ -1,6 +1,26 @@
+from Dinosaur import Dinosaur
+
 class Herd:
     def __init__(self):
         self.dinosaurs: List
 
     def create_herd(self):
 
+if __name__ == '__main__':
+    dino_one = Dinosaur()
+    dino_one.type: 'Bite'
+    dino_one.energy: 20
+    dino_one.attack_power: 10
+    dino_one.health: 30
+
+    dino_two = Dinosaur()
+    dino_two.type: 'Claw'
+    dino_two.energy: 15
+    dino_two.attack_power: 5
+    dino_two.health: 20
+
+    dino_three = Dinosaur()
+    dino_three.type: 'Claw'
+    dino_three.energy: 25
+    dino_three.attack_power: 15
+    dino_three.health: 25
